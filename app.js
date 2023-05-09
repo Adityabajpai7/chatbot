fe664c3d03b0bea1313846c6eef706b69d3fd6fa
Adityabajpai7 <133039988+Adityabajpai7@users.noreply.github.com>
2023-05-10 00:23:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 function talk() {
     var know = {
         "Who are you": "Hello, Aditya here ",
+        "hello": "hello,how are you",
+        "i am fine": "that's great",
+        "nice to meet you": "thank you nice to meet you too",
+        "hey": "hey,my friend",
+        "aditya": "at your service",
+        "good job": "my pleasure",
+        "what is ace?": "ace stands for association of computer science engineers",
         "How are you": "Good :)",
         "What can i do for you": "Search Aditya on Google>>Thank Me Later",
         "Your followers":
